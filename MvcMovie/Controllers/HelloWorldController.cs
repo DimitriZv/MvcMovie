1f2a8a2aa6eb36214fb3848f52bc6b39d5492de3
DimitriZv <71474008+DimitriZv@users.noreply.github.com>
2021-12-19 23:32:04 +1100

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers  ///[Controller]/[ActionName]/[Parameters]
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        //Index is called by default (if nothing specified)
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        //https://localhost:{PORT}/HelloWorld/Welcome/3?name=Rick
+        //pattern: "{controller=Home}/{action=Index}/{id?}");
+        //The ? (question mark) in the above URL is a separator, and the query string follows.
+        //The & character separates field-value pairs.
+        /*public string Welcome(string name, int ID = 1) //defaults to 1 if no value
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }*/
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {   //HelloWorld/Welcome?name=Rick&numtimes=4
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
